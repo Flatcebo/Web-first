671b0552647f2f90236d822e14e3703d11566961
bagdongseog <organicsic@naver.com>
2023-08-31 12:36:42 +0900

    second

--- app/FullPageScroll/page.tsx
+++ app/FullPageScroll/page.tsx
@@ -3,29 +3,119 @@
 import { useEffect, useRef } from "react";
 
 export default function Page() {
+  const DIVIDER_HEIGHT = 5;
   const outerRef = useRef<any>();
 
   useEffect(() => {
-    const wheelHandler = (e: React.WheelEvent<HTMLDivElement>) => {
-      e.preventDefault();
+    const wheelHandler = (e: React.WheelEvent) => {
+      // e.preventDefault();
       const { deltaY } = e;
       const { scrollTop } = outerRef.current;
       const pageHeight = window.innerHeight;
-      console.log(deltaY, scrollTop, pageHeight);
+
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          console.log("현재 1페이지, down");
+          outerRef.current.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log("현재 2페이지, down");
+          outerRef.current.scrollTo({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else {
+          // 현재 3페이지
+          console.log("현재 3페이지, down");
+          outerRef.current.scrollTo({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+      } else {
+        // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          console.log("현재 1페이지, up");
+          outerRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          console.log("현재 2페이지, up");
+          outerRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+        } else {
+          // 현재 3페이지
+          console.log("현재 3페이지, up");
+          outerRef.current.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+        }
+      }
+      console.log(pageHeight);
+      // if (deltaY > 0) {
+      //   console.log("up");
+      // } else {
+      //   console.log("down");
+      // }
+
+      // console.log(pageHeight, scrollTop);
+      // // 현재 페이지 계산
+      // const currentPage = Math.floor(scrollTop / pageHeight) + 1;
+
+      // let targetPage;
+      // if (deltaY > 0) {
+      //   // 스크롤 내릴 때
+      //   targetPage = Math.min(currentPage + 1, 3);
+      // } else {
+      //   // 스크롤 올릴 때
+      //   targetPage = Math.max(currentPage - 1, 1);
+      // }
+
+      // console.log(`현재 ${currentPage}페이지, ${deltaY > 0 ? "down" : "up"}`);
+
+      // outerRef.current.scrollTo({
+      //   top: (targetPage - 1) * pageHeight,
+      //   left: 0,
+      //   behavior: "smooth",
+      // });
     };
     const outerDivRefCurrent = outerRef.current;
-    outerDivRefCurrent.addEventListener("wheel", wheelHandler);
+    outerDivRefCurrent.addEventListener("wheel", wheelHandler, {
+      passive: false,
+    });
     return () => {
       outerDivRefCurrent.removeEventListener("wheel", wheelHandler);
     };
   }, []);
-  console.log("hihi");
 
   return (
     <div ref={outerRef}>
-      <div className="h-[100vh] bg-[red]">1</div>
-      <div className="h-[100vh] bg-[yellow]">2</div>
-      <div className="h-[100vh] bg-[blue]">3</div>
+      <div className="flex h-[100vh] justify-center items-center text-[100px] bg-[red]">
+        1
+      </div>
+      <div className="flex h-[100vh] justify-center items-center text-[100px] bg-[yellow]">
+        2
+      </div>
+      <div className="flex h-[100vh] justify-center items-center text-[100px] bg-[blue]">
+        3
+      </div>
     </div>
   );
 }
