6221a8cd0a25396fbeb65cdcf45e24e6ed818276
bagdongseog <organicsic@naver.com>
2023-09-19 17:21:04 +0900

    imgUpdate

--- app/FullPageScroll/page.tsx
+++ app/FullPageScroll/page.tsx
@@ -3,7 +3,7 @@
 import FloatingButton from "@/components/FloatingButton";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
-import Image from "@/node_modules/next/image";
+import Image, { ImageLoaderProps } from "@/node_modules/next/image";
 import React from "react";
 import { useEffect, useRef } from "react";
 import MountainIMG from "@/public/images/Mountain.jpeg";
@@ -11,25 +11,28 @@ import CafeIMG from "@/public/images/Cafe.jpeg";
 import FoodIMG from "@/public/images/Food.jpeg";
 import OceanIMG from "@/public/images/Ocean.jpeg";
 import SkyIMG from "@/public/images/Sky.jpeg";
-
-// import ReactFullPage from "@fullpag";
+import { imgLoader } from "@/utills/imgLoader";
 
 export default function Page() {
   const DIVIDER_HEIGHT = 5;
   const outerRef = useRef<any>();
+  const imgLoad = ({ src, width, quality }: ImageLoaderProps) =>
+    imgLoader({ src, width, quality });
 
   return (
     <div ref={outerRef} className="w-full h-full">
       <Header />
       <Sidebar />
       <div className="h-[100vh] w-full flex justify-center items-center">
+        {/* <img src="/images/Cafe.jpeg" width={500} height={300} as="public"/> */}
         <Image
           className="flex h-[100vh] w-[100vw]"
+          loader={imgLoad}
           src={MountainIMG}
           alt="Mountain"
-          // layout="reponsive"
           width={1980}
           height={1080}
+          priority
         />
         <div className="absolute h-[100vh] top-[13rem] flex flex-col justify-start items-center text-center gap-4">
           <p className="font-medium text-[2rem] text-center">무슨산</p>
@@ -44,36 +47,44 @@ export default function Page() {
       </div>
       <Image
         className="flex h-[100vh] w-[100vw]"
+        loader={imgLoad}
         src={OceanIMG}
         alt="Ocean"
         // layout="fixed"
         width={1980}
         height={1080}
+        priority
       />
 
       <Image
         className="flex h-[100vh] w-[100vw]"
+        loader={imgLoad}
         src={SkyIMG}
         alt="Sky"
         // layout="reponsive"
         width={1980}
         height={1080}
+        priority
       />
       <Image
         className="flex h-[100vh] w-[100vw]"
+        loader={imgLoad}
         src={FoodIMG}
         alt="Food"
         // layout="reponsive"
         width={1980}
         height={1080}
+        priority
       />
       <Image
         className="flex h-[100vh] w-[100vw]"
+        loader={imgLoad}
         src={CafeIMG}
         alt="Cafe"
         // layout="reponsive"
         width={1980}
         height={1080}
+        priority
       />
 
       <FloatingButton />
