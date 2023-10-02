9545976e59f24c9fcbdffc87c9ee644011c8a3d8
bagdongseog <organicsic@naver.com>
2023-10-02 20:43:55 +0900

    Modal Dev1

--- app/Parts/page.tsx
+++ app/Parts/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div className="w-[100vw] h-[100vh]">
       <div className="">
-        <div className="flex justify-center items-center text-center ">
+        <div className="flex justify-center items-center text-center">
           <Link
             href={"/"}
             className="text-[white] px-12 py-6 bg-[#ffffff50] hover:text-[#00000072] hover:bg-[#9a9a9a76]"
@@ -23,13 +23,13 @@ export default function Page() {
             openContentName="Sidebar"
             closedContentName="Close"
             onButtonStyle="z-50"
-            offButtonStyle="fixed h-[80vh] left-[30%] z-50 bg-[#ffffff50]"
+            offButtonStyle="fixed h-[80vh] left-[30%] z-50 bg-[#767676]"
             onButtonTextStyle="text-[12px] text-[white] px-10 py-5 hover:text-[#00000072] hover:bg-[#9a9a9a76]"
             offButtonTextStyle="text-[12px] text-[white] hover:text-[#00000072]"
           />
           <Modal
             onButtonStyle="text-[12px] text-[white] px-10 py-5 hover:text-[#00000072] hover:bg-[#9a9a9a76]"
-            offButtonStyle="text-[12px] text-[white] hover:text-[#00000072] right-0"
+            offButtonStyle="absolute text-[20px] text-[white] hover:text-[#00000072] right-[2%] top-[2%]"
           />
         </div>
       </div>
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -12,9 +12,38 @@ export default function Modal(props: IModalProps) {
   return (
     <div>
       {onModal ? (
-        <div className="w-full h-full items-center justify-center flex">
-          <div className="fixed top-[15%] w-[80vw] h-[70vh] bg-[#ffffff50] outline">
-            <form></form>
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="fixed top-[15%] w-[80vw] h-[70vh] bg-[#767676] text-[20px]">
+            <form className="m-[2%]">
+              <div className="text-center text-[26px] ">
+                <span>Sign Up</span>
+              </div>
+              {/* Form 1행 */}
+              <div className="w-[350px] flex">
+                <div className="mr-[5%]">
+                  <div className="flex flex-col text-center items-end">
+                    <span className="my-5">이름 : </span>
+                    <span className="my-5">이메일 : </span>
+                    <span className="my-5">전화번호 : </span>
+                  </div>
+                </div>
+                <div className="flex flex-col ">
+                  <input type="text" className="my-2 px-1 py-3 rounded-lg" />
+                  <input type="email" className="my-2 px-1 py-3 rounded-lg" />
+                  <input type="phone" className="my-2 px-1 py-3 rounded-lg" />
+                </div>
+              </div>
+              <div className="flex flex-col space-y-[3%]">
+                <div>
+                  <button
+                    type="submit"
+                    className="absolute right-[2%] bottom-[2%] text-[white] hover:text-[#00000072]"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </form>
             <div className="flex justify-end items-end">
               <button
                 className={props.offButtonStyle}
